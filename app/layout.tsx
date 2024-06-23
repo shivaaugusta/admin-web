3791f4424dd367bd035a745e082e6c8ebe310f1f
shivaaugusta <shivaaugusta98@gmail.com>
2024-06-23 15:59:36 +0700

    deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,18 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
+import './globals.css';
+import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
 
-import { ToasterProvider } from "@/providers/toast-provider";
-import { ModalProvider } from "@/providers/modal-provider";
+import { ToasterProvider } from '@/providers/toast-provider';
+import { ModalProvider } from '@/providers/modal-provider';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Ali Admin",
-  description: "Ali Admin",
+  title: 'Shiva Admin',
+  description: 'Shiva Admin',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <html lang="en">
--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
-import { useEffect, useState } from "react";
-import { Button } from "./button";
-import { ImagePlus, Trash } from "lucide-react";
-import Image from "next/image";
-import { CldUploadWidget } from "next-cloudinary";
+import { useEffect, useState } from 'react';
+import { Button } from './button';
+import { ImagePlus, Trash } from 'lucide-react';
+import Image from 'next/image';
+import { CldUploadWidget } from 'next-cloudinary';
 
 interface ImageUploadProps {
   disabled?: boolean;
@@ -13,12 +13,7 @@ interface ImageUploadProps {
   value: string[];
 }
 
-const ImageUpload: React.FC<ImageUploadProps> = ({
-  disabled,
-  onChange,
-  onRemove,
-  value,
-}) => {
+const ImageUpload: React.FC<ImageUploadProps> = ({ disabled, onChange, onRemove, value }) => {
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
@@ -37,17 +32,9 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
     <div>
       <div className="mb-4 flex items-center gap-4">
         {value.map((url) => (
-          <div
-            key={url}
-            className="relative w-[200px] h-[200px] rounded-md overflow-hidden"
-          >
+          <div key={url} className="relative w-[200px] h-[200px] rounded-md overflow-hidden">
             <div className="z-10 absolute top-2 right-2">
-              <Button
-                type="button"
-                onClick={() => onRemove(url)}
-                variant="destructive"
-                size="icon"
-              >
+              <Button type="button" onClick={() => onRemove(url)} variant="destructive" size="icon">
                 <Trash className="h-4 w-4" />
               </Button>
             </div>
@@ -55,18 +42,13 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="gtprd9am">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="gzu10nzd">
         {({ open }) => {
           const onClick = () => {
             open();
           };
           return (
-            <Button
-              type="button"
-              disabled={disabled}
-              variant="secondary"
-              onClick={onClick}
-            >
+            <Button type="button" disabled={disabled} variant="secondary" onClick={onClick}>
               <ImagePlus className="h-4 w-4 mr-2" />
               Upload image
             </Button>
